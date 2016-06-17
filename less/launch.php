edf3c4b9a72cc9e31ae28b110d9135752a15c982
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-06-17 18:48:07 +0700

    SIGH!

--- less/launch.php
+++ less/launch.php
@@ -1,12 +1,12 @@
 <?php
 
-require __DIR__ . DS . 'workers' . DS . 'lessphp' . DS . 'lessc.inc.php';
+require __DIR__ . DS . 'workers' . DS . 'less-php' . DS . 'lessc.inc.php';
 
 $c_less = $config->states->{'plugin_' . md5(File::B(__DIR__))};
 
 $less = new lessc;
 $less->setFormatter($c_less->formatterName);
-$less->setPreserveComments(true);
+$less->setPreserveComments(isset($c_less->preserveComments));
 
 function do_less_compile($content, $header) {
     global $less;
@@ -15,7 +15,7 @@ function do_less_compile($content, $header) {
         return $content;
     }
     if(strpos($content, '</style>') !== false) {
-        return preg_replace_callback('#(<style(?:>| .*?>))([\s\S]+?)(<\/style>)#', function($m) use($less) {
+        return preg_replace_callback('#(<style(?:>| .*?>)\s*)([\s\S]+?)(\s*<\/style>)#', function($m) use($less) {
             return $m[1] . $less->compile($m[2]) . $m[3];
         }, $content);
     }
